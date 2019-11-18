d8d6b8fe6eca97ee271b3ee3c813fd14af5eb7f1
Brandon Ihlein <43079494+Aceman2423@users.noreply.github.com>
2019-11-17 20:41:12 -0700

    Made changes to singleInsert, load data, and added multInsert
    Used a different connector PyMySQL and it fixes some of the authentification errors with load data. We need to allow the user we log in as to have file permissions for load data to fully work but works with localhost.

--- Phase 2/DatabaseConnector.py
+++ Phase 2/DatabaseConnector.py
@@ -1,14 +1,16 @@
 # Link to installing mysql for python https://www.w3schools.com/python/python_mysql_getstarted.asp
 # Link to simple select statement in python https://www.w3schools.com/python/python_mysql_select.asp
+# Link to download pymysql must pip both versions listed here -> https://pymysql.readthedocs.io/en/latest/user/installation.html
+import mysql.connector, pymysql, csv
 
-import mysql.connector
+
 
 mydb = mysql.connector.connect(
-    host="cs482project.ct5cpbeyrttk.us-east-1.rds.amazonaws.com",
-    # FILL IN VALUES, DO NOT PUSH INTO GITHUB PASSWORD AND USER
-    user="",
-    passwd="",
-    database="csproject"
+host="cs482project.ct5cpbeyrttk.us-east-1.rds.amazonaws.com",
+# FILL IN VALUES, DO NOT PUSH INTO GITHUB PASSWORD AND USER
+user="",
+passwd="",
+database=""
 )
 
 mycursor = mydb.cursor()
@@ -26,70 +28,123 @@ mycursor.close()
 mydb.close()
 
 
-# singleInsertPlayers
-# inserts a single line of data for players
-def singleInsertPlayers( id, firstName, lastName, teamId, position, touchdowns, totalyards, salary):
+#TEMPORARY FUNCTION - just used to delete the players/teams in the txt file for re-inserting tests
+def delete():
     try:
-        
-        mydb = mysql.connector.connect(
-            host="cs482project.ct5cpbeyrttk.us-east-1.rds.amazonaws.com",
-            # FILL IN VALUES, DO NOT PUSH INTO GITHUB PASSWORD AND USER
-            user="",
-            passwd="",
-            database="csproject"
-        )
+        db = pymysql.connect('cs482project.ct5cpbeyrttk.us-east-1.rds.amazonaws.com',
+                             # FILL IN VALUES, DO NOT PUSH INTO GITHUB PASSWORD AND USER
+                             '',
+                             '',
+                             '',
+                             local_infile=True)
+        mycursor = db.cursor()
+        delete = """DELETE FROM players WHERE PlayerID = 1 or PlayerID = 2 or PlayerID = 3 or PlayerID = 4 or PlayerID = 5"""
+        delete2 = """DELETE FROM teams WHERE TeamID = 123451 or TeamID = 789101 or TeamID = 111211 or TeamID = 314151"""
+        mycursor.execute(delete)
+        mycursor.execute(delete2)
+        db.commit()
+            
 
-        mycursor = mydb.cursor()
-        sql_single_insert = """INSERT INTO players (PlayerID,
-                                                    FirsName,
-                                                    LastName,
-                                                    TeamID,
-                                                    Position,
-                                                    Touchdowns,
-                                                    TotalYards,
-                                                    Salary) VALUES (%s, %s, %s, %s, %s, %s, %s, %s) """
-        insertTuple = ( id, firstName, lastName, teamId, position, touchdowns, totalyards, salary)
-        mycursor.execute(sql_single_insert, insertTuple)
-        mydb.commit()
-
-    except mysql.connector.Error as error:
+    except pymysql.InternalError as error:
         print("Failed to insert into MySql table{}".format(error))
 
     mycursor.close()
-    mydb.close()
-#### end of singleInsertPlayers
-    
+    db.close()
+##end of delete
 
-# loadDataInsertPlayers
-# load data insert from text file for table players
-def loadDataInsertPlayers( filePath ):
+
+## singlePlayerInsert
+## takes in the file and reads the lines then inserts each one individually
+def singlePlayersInsert( path ):
     try:
-        
-        mydb = mysql.connector.connect(
-            host="cs482project.ct5cpbeyrttk.us-east-1.rds.amazonaws.com",
-            # FILL IN VALUES, DO NOT PUSH INTO GITHUB PASSWORD AND USER
-            user="",
-            passwd="",
-            database="csproject"
-        )
-        
-        mycursor = mydb.cursor()
-        loadDataInsert = """LOAD DATA LOCAL INFILE '%s' INTO TABLE players FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n'"""
-        pathF = (loadDataInsert % filePath)
-        mycursor.execute(loadDataInsert % filePath)
-        mydb.commit()
+        db = pymysql.connect('cs482project.ct5cpbeyrttk.us-east-1.rds.amazonaws.com',
+                             # FILL IN VALUES, DO NOT PUSH INTO GITHUB PASSWORD AND USER
+                             '',
+                             '',
+                             '',
+                             local_infile=True)
+        mycursor = db.cursor()
+        with open(path, mode = 'r') as csv_file:
+            
+            csv_data=csv.reader(csv_file, delimiter=',')
+            for row in csv_data:
+                print(row[1])
+                print(len(row))
+                sql_single_insert = """INSERT INTO players (PlayerID,FirsName,LastName,TeamID,Position,Touchdowns,TotalYards,Salary) VALUES (%s, '%s', '%s', %s, '%s', %s, %s, %s)""" 
+                insert = sql_single_insert % (row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7])
+                print(insert)
+                mycursor.execute(insert)
+                db.commit()
+            
 
-    except mysql.connector.Error as error:
+    except pymysql.InternalError as error:
         print("Failed to insert into MySql table{}".format(error))
 
     mycursor.close()
-    mydb.close()
-### end of loadDataInsertPlayers
+    db.close()
+## end of singlePlayerInsert
+
+
+## multInsert
+## inserts multiple lines at once in to a given table
+def multInsert( path, table):
+    try:
+        db = pymysql.connect('cs482project.ct5cpbeyrttk.us-east-1.rds.amazonaws.com',
+                             # FILL IN VALUES, DO NOT PUSH INTO GITHUB PASSWORD AND USER
+                             '',
+                             '',
+                             '',
+                             local_infile=True)
+        mycursor = db.cursor()
+        with open(path, mode = 'r') as csv_file:
+            
+            csv_data=csv.reader(csv_file, delimiter=',')
+            list_tuples = []
+            for row in csv_data:
+                list_tuples.append(tuple(row))
+            values = ', '.join(map(str, list_tuples))
+            print(list_tuples)
+            temp = "INSERT INTO %s VALUES {}" % table
+            insert = temp.format(values)
+            mycursor.execute(insert)
+            db.commit()
+            
+            
+
+    except pymysql.InternalError as error:
+        print("Failed to insert into MySql table{}".format(error))
+
+    mycursor.close()
+    db.close()
+## end of multInsert
 
 
 
-#path = input("Enter file path: ")
-loadDataInsertPlayers("players.txt")
+## loadData function
+## loads data for any table and inserts
+def loadData( path, table ):
+    try:
+        db = pymysql.connect('cs482project.ct5cpbeyrttk.us-east-1.rds.amazonaws.com',
+                             # FILL IN VALUES, DO NOT PUSH INTO GITHUB PASSWORD AND USER
+                             '',
+                             '',
+                             '',
+                             local_infile=True)
+        mycursor= db.cursor()
+
+        # set local_infile to true just incase its not already
+        mycursor.execute("""SET GlOBAL local_infile = 1""")
+        db.commit()
+        loadDataInsert = """LOAD DATA LOCAL INFILE '%s' INTO TABLE %s FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n'"""
+        insertTuple = (path, table)
+        mycursor.execute(loadDataInsert % insertTuple)
+        db.commit()
+
+    except pymysql.InternalError as error:
+        print("Failed to insert into MySql table{}".format(error))
+    mycursor.close()
+    db.close()
+## end of loadDataPlayers
 
 
         
